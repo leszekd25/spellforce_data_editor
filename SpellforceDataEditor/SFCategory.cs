50c83fa7bfbd165510f4d28ca3b60d3bcea9cfba
Leszek Domaradzki <leszekd25@gmail.com>
2018-04-08 14:02:56 +0200

    create classes to deal with reading/writing different data from gamedata.cff - implementation will come later

---
+++ SpellforceDataEditor/SFCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellforceDataEditor
+{
+    public abstract class SFCategory
+    {
+        private string name;
+        private uint id;
+        private uint item_count;
+        private SFCategoryElement[] elements;
+        public abstract Object[] get_element(StreamReader sr);
+        public abstract SFVariant[] set_element(StreamWriter sw);
+        public abstract void read(StreamReader sr);
+        public abstract void write(StreamWriter sw);
+    }
+}
---
+++ SpellforceDataEditor/SFCategoryElement.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellforceDataEditor
+{
+    public enum TYPE { UByte, Byte, UShort, Short, UInt, Int, Float, String, Unknown };
+
+    public class SFVariant
+    {
+        public Object value;
+        public TYPE vtype;
+        public SFVariant()
+        {
+            vtype = TYPE.Unknown;
+            return;
+        }
+        public void set(Object obj)
+        {
+            value = obj;
+            Type t = obj.GetType();
+            if (t.Equals(typeof(Byte)))
+            {
+                vtype = TYPE.UByte;
+                return;
+            }
+            if (t.Equals(typeof(SByte)))
+            {
+                vtype = TYPE.Byte;
+                return;
+            }
+            if (t.Equals(typeof(Int16)))
+            {
+                vtype = TYPE.Short;
+                return;
+            }
+            if (t.Equals(typeof(UInt16)))
+            {
+                vtype = TYPE.UShort;
+                return;
+            }
+            if (t.Equals(typeof(Int32)))
+            {
+                vtype = TYPE.Int;
+                return;
+            }
+            if (t.Equals(typeof(UInt32)))
+            {
+                vtype = TYPE.UInt;
+                return;
+            }
+            if (t.Equals(typeof(float)))
+            {
+                vtype = TYPE.Float;
+                return;
+            }
+            if (t.Equals(typeof(string)))
+            {
+                vtype = TYPE.String;
+                return;
+            }
+            vtype = TYPE.Unknown;
+        }
+    }
+
+    public abstract class SFCategoryElement
+    {
+        protected SFVariant[] properties;
+        public SFCategoryElement()
+        {
+            properties = null;
+        }
+        public void set(Object[] objs)
+        {
+            properties = new SFVariant[objs.Length];
+            for (int i = 0; i < objs.Length; i++)
+            {
+                properties[i] = new SFVariant();
+                properties[i].set(objs[i]);
+            }
+            return;
+        }
+        public SFVariant[] get()
+        {
+            return properties;
+        }
+    }
+}
