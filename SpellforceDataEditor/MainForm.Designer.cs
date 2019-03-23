dfc801e0ade85ffb9fbf7cc64809b759d25469e0
leszekd25 <leszekd25@gmail.com>
2019-03-23 17:56:07 +0100

    Updated version number

--- SpellforceDataEditor/MainForm.Designer.cs
+++ SpellforceDataEditor/MainForm.Designer.cs
@@ -108,7 +108,7 @@
             this.labelVersion.Name = "labelVersion";
             this.labelVersion.Size = new System.Drawing.Size(108, 13);
             this.labelVersion.TabIndex = 6;
-            this.labelVersion.Text = "Version 22.01.2019.1";
+            this.labelVersion.Text = "Version 23.03.2019.1";
             // 
             // linkEditor
             // 
